e2205067f9fb040dac68386938c039b8939e3207
4leaf.peb <4leaf.peb@gmail.com>
2022-10-07 14:35:06 +0900

    modfiy

--- front/reducers/admission.js
+++ front/reducers/admission.js
@@ -4,65 +4,247 @@ export const initailState = {
   admissionList: [],
 
   //
-  st_admissionListLoading: false, // admission 가져오기
-  st_admissionListDone: false,
-  st_admissionListError: null,
-
+  st_admissionAllListLoading: false, // 이용현황 전체 가져오기
+  st_admissionAllListDone: false,
+  st_admissionAllListError: null,
   //
-  st_admissionUpdateLoading: false, // admission 수정하기
-  st_admissionUpdateDone: false,
-  st_admissionUpdateError: null,
+  st_admissionNormalListLoading: false, // 이용현황 일반 가져오기
+  st_admissionNormalListDone: false,
+  st_admissionNormalListError: null,
+  //
+  st_admissionDementiaListLoading: false, // 이용현황 치매전담 가져오기
+  st_admissionDementiaListDone: false,
+  st_admissionDementiaListError: null,
+  //
+  st_admissionWeekListLoading: false, // 이용현황 주간 가져오기
+  st_admissionWeekListDone: false,
+  st_admissionWeekListError: null,
+  //
+  st_admissionAllUpdateLoading: false, // 이용현황 전체 가져오기
+  st_admissionAllUpdateDone: false,
+  st_admissionAllUpdateError: null,
+  //
+  st_admissionNormalUpdateLoading: false, // 이용현황 일반 가져오기
+  st_admissionNormalUpdateDone: false,
+  st_admissionNormalUpdateError: null,
+  //
+  st_admissionDementiaUpdateLoading: false, // 이용현황 치매전담 가져오기
+  st_admissionDementiaUpdateDone: false,
+  st_admissionDementiaUpdateError: null,
+  //
+  st_admissionWeekUpdateLoading: false, // 이용현황 주간 가져오기
+  st_admissionWeekUpdateDone: false,
+  st_admissionWeekUpdateError: null,
 };
 
-export const ADMISSION_LIST_REQUEST = "ADMISSION_LIST_REQUEST";
-export const ADMISSION_LIST_SUCCESS = "ADMISSION_LIST_SUCCESS";
-export const ADMISSION_LIST_FAILURE = "ADMISSION_LIST_FAILURE";
+export const ADMISSION_ALL_LIST_REQUEST = "ADMISSION_ALL_LIST_REQUEST";
+export const ADMISSION_ALL_LIST_SUCCESS = "ADMISSION_ALL_LIST_SUCCESS";
+export const ADMISSION_ALL_LIST_FAILURE = "ADMISSION_ALL_LIST_FAILURE";
 
-export const ADMISSION_UPDATE_REQUEST = "ADMISSION_UPDATE_REQUEST";
-export const ADMISSION_UPDATE_SUCCESS = "ADMISSION_UPDATE_SUCCESS";
-export const ADMISSION_UPDATE_FAILURE = "ADMISSION_UPDATE_FAILURE";
+export const ADMISSION_NORMAL_LIST_REQUEST = "ADMISSION_NORMAL_LIST_REQUEST";
+export const ADMISSION_NORMAL_LIST_SUCCESS = "ADMISSION_NORMAL_LIST_SUCCESS";
+export const ADMISSION_NORMAL_LIST_FAILURE = "ADMISSION_LIST_FAILURE";
+
+export const ADMISSION_DEMENTIA_LIST_REQUEST =
+  "ADMISSION_DEMENTIA_LIST_REQUEST";
+export const ADMISSION_DEMENTIA_LIST_SUCCESS =
+  "ADMISSION_DEMENTIA_LIST_SUCCESS";
+export const ADMISSION_DEMENTIA_LIST_FAILURE =
+  "ADMISSION_DEMENTIA_LIST_FAILURE";
+
+export const ADMISSION_WEEK_LIST_REQUEST = "ADMISSION_WEEK_LIST_REQUEST";
+export const ADMISSION_WEEK_LIST_SUCCESS = "ADMISSION_WEEK_LIST_SUCCESS";
+export const ADMISSION_WEEK_LIST_FAILURE = "ADMISSION_WEEK_LIST_FAILURE";
+
+export const ADMISSION_ALL_UPDATE_REQUEST = "ADMISSION_ALL_UPDATE_REQUEST";
+export const ADMISSION_ALL_UPDATE_SUCCESS = "ADMISSION_ALL_UPDATE_SUCCESS";
+export const ADMISSION_ALL_UPDATE_FAILURE = "ADMISSION_ALL_UPDATE_FAILURE";
+
+export const ADMISSION_NORMAL_UPDATE_REQUEST =
+  "ADMISSION_NORMAL_UPDATE_REQUEST";
+export const ADMISSION_NORMAL_UPDATE_SUCCESS =
+  "ADMISSION_NORMAL_UPDATE_SUCCESS";
+export const ADMISSION_NORMAL_UPDATE_FAILURE =
+  "ADMISSION_NORMAL_UPDATE_FAILURE";
+
+export const ADMISSION_DEMENTIA_UPDATE_REQUEST =
+  "ADMISSION_DEMENTIA_UPDATE_REQUEST";
+export const ADMISSION_DEMENTIA_UPDATE_SUCCESS =
+  "ADMISSION_DEMENTIA_UPDATE_SUCCESS";
+export const ADMISSION_DEMENTIA_UPDATE_FAILURE =
+  "ADMISSION_DEMENTIA_UPDATE_FAILURE";
+
+export const ADMISSION_WEEK_UPDATE_REQUEST = "ADMISSION_WEEK_UPDATE_REQUEST";
+export const ADMISSION_WEEK_UPDATE_SUCCESS = "ADMISSION_WEEK_UPDATE_SUCCESS";
+export const ADMISSION_WEEK_UPDATE_FAILURE = "ADMISSION_WEEK_UPDATE_FAILURE";
 
 const reducer = (state = initailState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
-      case ADMISSION_LIST_REQUEST: {
-        draft.st_admissionListLoading = true;
-        draft.st_admissionListDone = false;
-        draft.st_admissionListError = null;
+      case ADMISSION_ALL_LIST_REQUEST: {
+        draft.st_admissionAllListLoading = true;
+        draft.st_admissionAllListDone = false;
+        draft.st_admissionAllListError = null;
         break;
       }
-      case ADMISSION_LIST_SUCCESS: {
-        draft.st_admissionListLoading = false;
-        draft.st_admissionListDone = true;
-        draft.st_admissionListError = null;
+      case ADMISSION_ALL_LIST_SUCCESS: {
+        draft.st_admissionAllListLoading = false;
+        draft.st_admissionAllListDone = true;
+        draft.st_admissionAllListError = null;
         draft.admissionList = action.data;
         break;
       }
-      case ADMISSION_LIST_FAILURE: {
-        draft.st_admissionListLoading = false;
-        draft.st_admissionListDone = false;
-        draft.st_admissionListError = action.error;
+      case ADMISSION_ALL_LIST_FAILURE: {
+        draft.st_admissionAllListLoading = false;
+        draft.st_admissionAllListDone = false;
+        draft.st_admissionAllListError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+      case ADMISSION_NORMAL_LIST_REQUEST: {
+        draft.st_admissionNormalListLoading = true;
+        draft.st_admissionNormalListDone = false;
+        draft.st_admissionNormalListError = null;
+        break;
+      }
+      case ADMISSION_NORMAL_LIST_SUCCESS: {
+        draft.st_admissionNormalListLoading = false;
+        draft.st_admissionNormalListDone = true;
+        draft.st_admissionNormalListError = null;
+        draft.admissionList = action.data;
+        break;
+      }
+      case ADMISSION_NORMAL_LIST_FAILURE: {
+        draft.st_admissionNormalListLoading = false;
+        draft.st_admissionNormalListDone = false;
+        draft.st_admissionNormalListError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+      case ADMISSION_DEMENTIA_LIST_REQUEST: {
+        draft.st_admissionDementiaListLoading = true;
+        draft.st_admissionDementiaListDone = false;
+        draft.st_admissionDementiaListError = null;
+        break;
+      }
+      case ADMISSION_DEMENTIA_LIST_SUCCESS: {
+        draft.st_admissionDementiaListLoading = false;
+        draft.st_admissionDementiaListDone = true;
+        draft.st_admissionDementiaListError = null;
+        draft.admissionList = action.data;
+        break;
+      }
+      case ADMISSION_DEMENTIA_LIST_FAILURE: {
+        draft.st_admissionDementiaListLoading = false;
+        draft.st_admissionDementiaListDone = false;
+        draft.st_admissionDementiaListError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+      case ADMISSION_WEEK_LIST_REQUEST: {
+        draft.st_admissionWeekListLoading = true;
+        draft.st_admissionWeekListDone = false;
+        draft.st_admissionWeekListError = null;
+        break;
+      }
+      case ADMISSION_WEEK_LIST_SUCCESS: {
+        draft.st_admissionWeekListLoading = false;
+        draft.st_admissionWeekListDone = true;
+        draft.st_admissionWeekListError = null;
+        draft.admissionList = action.data;
+        break;
+      }
+      case ADMISSION_WEEK_LIST_FAILURE: {
+        draft.st_admissionWeekListLoading = false;
+        draft.st_admissionWeekListDone = false;
+        draft.st_admissionWeekListError = action.error;
         break;
       }
 
       //////////////////////////////////////////////
 
-      case ADMISSION_UPDATE_REQUEST: {
-        draft.st_admissionUpdateLoading = true;
-        draft.st_admissionUpdateDone = false;
-        draft.st_admissionUpdateError = null;
+      case ADMISSION_ALL_UPDATE_REQUEST: {
+        draft.st_admissionAllUpdateLoading = true;
+        draft.st_admissionAllUpdateDone = false;
+        draft.st_admissionAllUpdateError = null;
         break;
       }
-      case ADMISSION_UPDATE_SUCCESS: {
-        draft.st_admissionUpdateLoading = false;
-        draft.st_admissionUpdateDone = true;
-        draft.st_admissionUpdateError = null;
+      case ADMISSION_ALL_UPDATE_SUCCESS: {
+        draft.st_admissionAllUpdateLoading = false;
+        draft.st_admissionAllUpdateDone = true;
+        draft.st_admissionAllUpdateError = null;
         break;
       }
-      case ADMISSION_UPDATE_FAILURE: {
-        draft.st_admissionUpdateLoading = false;
-        draft.st_admissionUpdateDone = false;
-        draft.st_admissionUpdateError = action.error;
+      case ADMISSION_ALL_UPDATE_FAILURE: {
+        draft.st_admissionAllUpdateLoading = false;
+        draft.st_admissionAllUpdateDone = false;
+        draft.st_admissionAllUpdateError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+
+      case ADMISSION_NORMAL_UPDATE_REQUEST: {
+        draft.st_admissionNormalUpdateLoading = true;
+        draft.st_admissionNormalUpdateDone = false;
+        draft.st_admissionNormalUpdateError = null;
+        break;
+      }
+      case ADMISSION_NORMAL_UPDATE_SUCCESS: {
+        draft.st_admissionNormalUpdateLoading = false;
+        draft.st_admissionNormalUpdateDone = true;
+        draft.st_admissionNormalUpdateError = null;
+        break;
+      }
+      case ADMISSION_NORMAL_UPDATE_FAILURE: {
+        draft.st_admissionNormalUpdateLoading = false;
+        draft.st_admissionNormalUpdateDone = false;
+        draft.st_admissionNormalUpdateError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+
+      case ADMISSION_DEMENTIA_UPDATE_REQUEST: {
+        draft.st_admissionDementiaUpdateLoading = true;
+        draft.st_admissionDementiaUpdateDone = false;
+        draft.st_admissionDementiaUpdateError = null;
+        break;
+      }
+      case ADMISSION_DEMENTIA_UPDATE_SUCCESS: {
+        draft.st_admissionDementiaUpdateLoading = false;
+        draft.st_admissionDementiaUpdateDone = true;
+        draft.st_admissionDementiaUpdateError = null;
+        break;
+      }
+      case ADMISSION_DEMENTIA_UPDATE_FAILURE: {
+        draft.st_admissionDementiaUpdateLoading = false;
+        draft.st_admissionDementiaUpdateDone = false;
+        draft.st_admissionDementiaUpdateError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////
+
+      case ADMISSION_WEEK_UPDATE_REQUEST: {
+        draft.st_admissionWeekUpdateLoading = true;
+        draft.st_admissionWeekUpdateDone = false;
+        draft.st_admissionWeekUpdateError = null;
+        break;
+      }
+      case ADMISSION_WEEK_UPDATE_SUCCESS: {
+        draft.st_admissionWeekUpdateLoading = false;
+        draft.st_admissionWeekUpdateDone = true;
+        draft.st_admissionWeekUpdateError = null;
+        break;
+      }
+      case ADMISSION_WEEK_UPDATE_FAILURE: {
+        draft.st_admissionWeekUpdateLoading = false;
+        draft.st_admissionWeekUpdateDone = false;
+        draft.st_admissionWeekUpdateError = action.error;
         break;
       }
 
--- front/sagas/admission.js
+++ front/sagas/admission.js
@@ -1,34 +1,57 @@
 import { all, call, delay, fork, put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 import {
-  ADMISSION_LIST_REQUEST,
-  ADMISSION_LIST_SUCCESS,
-  ADMISSION_LIST_FAILURE,
-
+  ADMISSION_ALL_LIST_REQUEST,
+  ADMISSION_ALL_LIST_SUCCESS,
+  ADMISSION_ALL_LIST_FAILURE,
   //
-  ADMISSION_UPDATE_REQUEST,
-  ADMISSION_UPDATE_SUCCESS,
-  ADMISSION_UPDATE_FAILURE,
+  ADMISSION_NORMAL_LIST_REQUEST,
+  ADMISSION_NORMAL_LIST_SUCCESS,
+  ADMISSION_NORMAL_LIST_FAILURE,
+  //
+  ADMISSION_DEMENTIA_LIST_REQUEST,
+  ADMISSION_DEMENTIA_LIST_SUCCESS,
+  ADMISSION_DEMENTIA_LIST_FAILURE,
+  //
+  ADMISSION_WEEK_LIST_REQUEST,
+  ADMISSION_WEEK_LIST_SUCCESS,
+  ADMISSION_WEEK_LIST_FAILURE,
+  //
+  ADMISSION_ALL_UPDATE_REQUEST,
+  ADMISSION_ALL_UPDATE_SUCCESS,
+  ADMISSION_ALL_UPDATE_FAILURE,
+  //
+  ADMISSION_NORMAL_UPDATE_REQUEST,
+  ADMISSION_NORMAL_UPDATE_SUCCESS,
+  ADMISSION_NORMAL_UPDATE_FAILURE,
+  //
+  ADMISSION_DEMENTIA_UPDATE_REQUEST,
+  ADMISSION_DEMENTIA_UPDATE_SUCCESS,
+  ADMISSION_DEMENTIA_UPDATE_FAILURE,
+  //
+  ADMISSION_WEEK_UPDATE_REQUEST,
+  ADMISSION_WEEK_UPDATE_SUCCESS,
+  ADMISSION_WEEK_UPDATE_FAILURE,
 } from "../reducers/admission";
 
 // SAGA AREA ********************************************************************************************************
 // ******************************************************************************************************************
-async function admissionListAPI(data) {
-  return await axios.post(`/api/admission/list`, data);
+async function admissionAllListAPI(data) {
+  return await axios.post(`/api/admission/all/list`, data);
 }
 
 function* admissionList(action) {
   try {
-    const result = yield call(admissionListAPI, action.data);
+    const result = yield call(admissionAllListAPI, action.data);
 
     yield put({
-      type: ADMISSION_LIST_SUCCESS,
+      type: ADMISSION_ALL_LIST_SUCCESS,
       data: result.data,
     });
   } catch (err) {
     console.error(err);
     yield put({
-      type: ADMISSION_LIST_FAILURE,
+      type: ADMISSION_ALL_LIST_FAILURE,
       error: err.response.data,
     });
   }
@@ -36,22 +59,160 @@ function* admissionList(action) {
 
 // SAGA AREA ********************************************************************************************************
 // ******************************************************************************************************************
-async function admissionUpdateAPI(data) {
-  return await axios.post(`/api/admission/update`, data);
+async function admissionDementiaListAPI(data) {
+  return await axios.post(`/api/admission/dementia/list`, data);
 }
 
-function* admissionUpdate(action) {
+function* admissionDementiaList(action) {
   try {
-    const result = yield call(admissionUpdateAPI, action.data);
+    const result = yield call(admissionDementiaListAPI, action.data);
 
     yield put({
-      type: ADMISSION_UPDATE_SUCCESS,
+      type: ADMISSION_DEMENTIA_LIST_SUCCESS,
       data: result.data,
     });
   } catch (err) {
     console.error(err);
     yield put({
-      type: ADMISSION_UPDATE_FAILURE,
+      type: ADMISSION_DEMENTIA_LIST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionNormalListAPI(data) {
+  return await axios.post(`/api/admission/normal/list`, data);
+}
+
+function* admissionNoramlList(action) {
+  try {
+    const result = yield call(admissionNormalListAPI, action.data);
+
+    yield put({
+      type: ADMISSION_NORMAL_LIST_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_NORMAL_LIST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionWeekListAPI(data) {
+  return await axios.post(`/api/admission/week/list`, data);
+}
+
+function* admissionWeekList(action) {
+  try {
+    const result = yield call(admissionWeekListAPI, action.data);
+
+    yield put({
+      type: ADMISSION_WEEK_LIST_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_WEEK_LIST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionAllUpdateAPI(data) {
+  return await axios.post(`/api/admission/all/update`, data);
+}
+
+function* admissionAllUpdate(action) {
+  try {
+    const result = yield call(admissionAllUpdateAPI, action.data);
+
+    yield put({
+      type: ADMISSION_ALL_UPDATE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_ALL_UPDATE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionNormalUpdateAPI(data) {
+  return await axios.post(`/api/admission/normal/update`, data);
+}
+
+function* admissionNormalUpdate(action) {
+  try {
+    const result = yield call(admissionNormalUpdateAPI, action.data);
+
+    yield put({
+      type: ADMISSION_NORMAL_UPDATE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_NORMAL_UPDATE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionDementiaUpdateAPI(data) {
+  return await axios.post(`/api/admission/dementia/update`, data);
+}
+
+function* admissionDementiaUpdate(action) {
+  try {
+    const result = yield call(admissionDementiaUpdateAPI, action.data);
+
+    yield put({
+      type: ADMISSION_DEMENTIA_UPDATE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_DEMENTIA_UPDATE_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+async function admissionWeekUpdateAPI(data) {
+  return await axios.post(`/api/admission/week/update`, data);
+}
+
+function* admissionWeekUpdate(action) {
+  try {
+    const result = yield call(admissionWeekUpdateAPI, action.data);
+
+    yield put({
+      type: ADMISSION_WEEK_UPDATE_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: ADMISSION_WEEK_UPDATE_FAILURE,
       error: err.response.data,
     });
   }
@@ -62,19 +223,49 @@ function* admissionUpdate(action) {
 // ******************************************************************************************************************
 
 //////////////////////////////////////////////////////////////
-function* watchAdmissionList() {
-  yield takeLatest(ADMISSION_LIST_REQUEST, admissionList);
+function* watchAdmissionAllList() {
+  yield takeLatest(ADMISSION_ALL_LIST_REQUEST, admissionList);
 }
 
-function* watchAdmissionUpdate() {
-  yield takeLatest(ADMISSION_UPDATE_REQUEST, admissionUpdate);
+function* watchAdmissionNormalList() {
+  yield takeLatest(ADMISSION_NORMAL_LIST_REQUEST, admissionNoramlList);
+}
+
+function* watchAdmissionDementiaList() {
+  yield takeLatest(ADMISSION_DEMENTIA_LIST_REQUEST, admissionDementiaList);
+}
+
+function* watchAdmissionWeekList() {
+  yield takeLatest(ADMISSION_WEEK_LIST_REQUEST, admissionWeekList);
+}
+
+function* watchAdmissionAllUpdate() {
+  yield takeLatest(ADMISSION_ALL_UPDATE_REQUEST, admissionAllUpdate);
+}
+
+function* watchAdmissionNormalUpdate() {
+  yield takeLatest(ADMISSION_NORMAL_UPDATE_REQUEST, admissionNormalUpdate);
+}
+
+function* watchAdmissionDementiaUpdate() {
+  yield takeLatest(ADMISSION_DEMENTIA_UPDATE_REQUEST, admissionDementiaUpdate);
+}
+
+function* watchAdmissionWeekUpdate() {
+  yield takeLatest(ADMISSION_WEEK_UPDATE_REQUEST, admissionWeekUpdate);
 }
 
 //////////////////////////////////////////////////////////////
 export default function* admissionSaga() {
   yield all([
-    fork(watchAdmissionList),
-    fork(watchAdmissionUpdate),
+    fork(watchAdmissionAllList),
+    fork(watchAdmissionNormalList),
+    fork(watchAdmissionDementiaList),
+    fork(watchAdmissionWeekList),
+    fork(watchAdmissionAllUpdate),
+    fork(watchAdmissionNormalUpdate),
+    fork(watchAdmissionDementiaUpdate),
+    fork(watchAdmissionWeekUpdate),
 
     //
   ]);
